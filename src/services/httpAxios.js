54153b981db1464837cca5f84e4052d8975f5760
Dhiraj Pandya <115932767+pandyadhiraj@users.noreply.github.com>
2024-02-25 20:15:21 +0530

    Added backend url to axios

--- src/services/httpAxios.js
+++ src/services/httpAxios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const httpAxios = axios.create({
-  baseURL: "http://localhost:1337",
+  //baseURL: "http://localhost:1337",
+  baseURL: "https://internship-portal-backend-l0hx.onrender.com",
   withCredentials: "true",
 });
 
